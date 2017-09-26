532c356424a7ab558b19593001ac178accb21e31
Matt Snow <matthew.j.snow@gmail.com>
2017-09-25 18:01:14 -0600

    divisions and tournaments controllers

--- app/controllers/api/divisions_controller.rb
+++ app/controllers/api/divisions_controller.rb
@@ -1,20 +1,42 @@
 class Api::DivisionsController < ApplicationController
-  
+  before_action :set_tournament
+  before_action :set_division, only: [:update, :destroy]
+
+  def index
+    render :json => Division.all.to_json(:include => { :category => { :only => :id }})
+  end
+
   def create
-    @tournament = Tournament.find(params[:task][:tournament])
     division = @tournament.divisions.new(division_params)
-    if division.save 
-      render json: division 
-    else 
+    if division.save
+      render json: division.to_json(:include => { :category => { :only => :id }})
+    else
       render json: { errors: division.errors.full_messages.join(',') }, status: 422
-    end 
+    end
   end
 
   def update
+    if @division.update(division_params)
+      render json: division.to_json(:include => { :category => { :only => :id }})
+    else
+      render json: { errors: division.errors.full_messages.join(', ')}, status: 422
+    end
   end
 
-  private 
-  def task_params
+  def destroy
+    @division.destroy
+  end
+
+  private
+  def division_params
     params.require(:division).permit(:name, :competition_type, :number_of_teams)
+  end
+
+  def set_tournament
+    @tournament = Tournament.find(params[:division][:tournament])
+  end
+
+  def set_division
+    @division = Division.find(params[:id])
   end
 end
--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -1,24 +1,39 @@
 class Api::TournamentsController < ApplicationController
+  before_action :set_tournament, only: [:update, :destroy]
+
+  def index
+    tournaments = Tournament.all
+    render json: tournaments
+  end
+
   def create
     tournament = Tournament.new
-
     if tournament.save(tournament_params)
       render json: tournament
     else
       render json: { errors: tournament.errors.join(',') }, status: 422
-    end 
+    end
 
   end
 
   def update
+    if @tournament.update(tournament_params)
+      render json: @tournament
+    else
+      render json: { errors: @tournament.errors.join(', ')}, status: 422
   end
 
   def destroy
-  end 
+    @tournament.destroy
+  end
 
   private
   def tournament_params
-    tournament.require(:tournament).permit(:name)
+    params.require(:tournament).permit(:name)
+  end
+
+  def set_tournament
+    @tournament = Tournament.find(params[:id])
   end
 
 end
